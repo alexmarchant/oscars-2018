d52df2bc6178f140506c860d0cfbb26eba4f6a83
Alex Marchant <alexjmarchant@gmail.com>
2018-01-29 17:46:44 -0500

    Scroll to bottom when the component mounts

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -26,6 +26,7 @@ class Chat extends Component {
 
   componentDidMount() {
     this.props.dispatch(startListeningForChatMessagesUpdates())
+    this.scrollToBottom()
   }
 
   componentWillUnmount() {
